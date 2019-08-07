e640fe3839ca2fea00770c4504253b257eddaae9
Nick Fabry <nick.fabry@coredump.us>
2019-08-07 18:30:09 -0500

    Initial commit after template clone.

--- README.txt
+++ README.txt
@@ -1,41 +1,18 @@
-This is my attempt to provide a decent template for python CLI based projects, with the sample name, envy. In order to use this, one should copy this entire directory structure to a new directory, EXCEPT /.venv . The purpose of /.venv is a location to store a python venv; by its nature, it is environment dependent, and thus should not be included in either git repositories or other syncing mechanisms. The .gitignore file excludes the /.venv directory.
+xl_strip_null_rows is designed to solve a specific problem - Excel sheets with
+a huge number of blank rows after the real data, often up to the maximum
+allowable number of rows, 1,048,576. Stripping out these empty rows in Excel
+is often difficult or impossible because of the massive size in memory such an
+Excel file takes, slowing manipulations to a crawl. This program aims to do
+that, stripping out blank rows in a much more efficient fashion, approximately
+100x as fast as in Excel.
 
-So, to set up a new python project in venv, do the following:
-
-- Copy the top level envy directory into your new project directory, or, clone it from the envy git repo.
-
-- If you've cloned it, delete the .git directory so there is no association with the original template.
-
-- Create a new repo in GitHub
-(Click Stuff)
-
-- Create a local git repo
-echo "# project_name" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-
-- Now attach and push the git repo
-git remote add origin https://github.com/NickFabry/project_name.git
-git push -u origin master
-
-The project file part is done. Now, we must set up a venv.
-
-- Create the venv in the directory with the following command:
-python -m venv --prompt project_name /path_to_project/project_name/venv
-
-- Activate the virtual environment:
-cd /path_to_project/project_name/venv; source bin/activate
-
-- Install dependencies:
-pip install pandas
-
-- Put a soft link in bin to your main runner script:
-ln -s bin/run_my_script project_name/main.py
-
-- Deactivate the environment
-deactivate
-
-
-
-Hopefully you're done now!
+The algorthim used is limited. It iterates over all the Worksheets in the
+Workbook. In each Worksheet, it reads each cell in the first column
+sequentially from top to bottom. When it finds the FIRST blank/null cell, it
+then copies the all the rows above that blank/null cell, and puts them in a
+brand-new Workbook and identically named Worksheet. Note that all formatting,
+formulas, charts, etc. are not preserved in the copy; only the values. Note
+also that a stray blank row will cause the program to ignore all the rows
+(even if non-blank) below it. It also makes no attempt to strip away or detect
+empty columns. In the future, we may add additional options for these use
+cases.
---
+++ python/main.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+
+""" xl_strip_null_rows is designed to solve a specific problem - Excel sheets witha huge number of blank rows after the real data, often up to the maximum allowable number of rows, 1,048,576. Stripping out these empty rows in Excel is often difficult or impossible because of the massive size in memory such an Excel file takes, slowing manipulations to a crawl. This program aims to do that, stripping out blank rows in a much more efficient fashion, approximately 100x as fast as in Excel.
+
+The algorthim used is limited. It iterates over all the Worksheets in the Workbook. In each Worksheet, it reads each cell in the first column sequentially from top to bottom. When it finds the FIRST blank/null cell, it then copies the all the rows above that blank/null cell, and puts them in a brand-new Workbook and identically named Worksheet. Note that all formatting,formulas, charts, etc. are not preserved in the copy; only the values. Note also that a stray blank row will cause the program to ignore all the rows(even if non-blank) below it. It also makes no attempt to strip away or detect empty columns. In the future, we may add additional options for these usecases.
+"""
+
+
+import sys, os
+import argparse
+import openpyxl
+
+
+
+def arg_parser():
+    parser = argparse.ArgumentParser(
+        description='This program reads the provided Excel Workbook specified by --input, strips out all the rows after the first blank row, and writes the remaining rows to a new Excel Workbook. Note that everything (e.g. formatting, charts, etc.) other than the values are not preserved.')
+    parser.add_argument('-i', '--input', metavar='FILE', 
+        help='The Excel file you wish to strip blank rows from.')
+    parser.add_argument('-o', '--output', metavar='FILE',
+        help='The path you wish to write the output Excel file to.')
+    return parser
+
+
+
+### MAIN ###
+def main(args):
+    iwb = openpyxl.load_workbook(args.input, read_only=True)
+    owb = openpyxl.Workbook(write_only=True)
+    for sn in iwb.sheetnames: 
+        ows = owb.create_sheet(title=sn)
+        i = 0
+        for a_row in iwb[sn].iter_rows():
+            if a_row[0].value is None:
+                print(f'Sheet {sn} done at row {i}.')
+                break
+            else:
+                row_vals = [c.value for c in a_row]
+                ows.append(row_vals)
+                i += 1
+    print(f'Writing Workbook: {args.output}')
+    owb.save(args.output)
+    return locals()
+### END MAIN ###
+
+
+
+### MAIN RUNNER ###
+if __name__ == '__main__':
+    args = arg_parser().parse_args()
+    print(args)
+    if sys.__stdin__.isatty():
+        locals().update(main(args))
+    else:
+        main(args)
+### END ###
